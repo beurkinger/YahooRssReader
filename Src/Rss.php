a6965e9707322080261afd85ee4edfac38428954
beurkinger <tgoehringer@gmail.com>
2017-05-04 13:17:07 +0200

    css and php corrections

--- Src/Rss.php
+++ Src/Rss.php
@@ -11,38 +11,48 @@ class Rss
 {
     const DEF_HEADER = 'Xml-type: text/xml ; charset=utf-8';
     const MAX_NB_TRY = 10;
-    
+
     private $sourceUrl;
     private $header;
     private $xml;
-    
+
     function __construct($sourceUrl, $header = self::DEF_HEADER)
     {
         $this->setSourceUrl($sourceUrl);
         $this->setHeader($header);
     }
-    
+
     function getXmlFile()
     {
         header($this->getHeader());
         echo $this->getXML()->asXml();
     }
-    
+
     function retrieveXml()
     {
         libxml_use_internal_errors(true);
         $tries = 0;
+        $ctx = stream_context_create([
+          'http' => [
+            'timeout' => 1
+          ]
+        ]);
         do
         {
-            $string = file_get_contents($this->getSourceUrl());
+          try {
+            $tries++;
+            $string = file_get_contents($this->getSourceUrl(), 0, $ctx);
             $xml = simplexml_load_string($string);
+          } catch (Exception $e) {
+            $xml = false;
+          }
         }
         while($xml === false && $tries <= self::MAX_NB_TRY);
-        
+
         $this->setXml($xml);
         return $this;
     }
-    
+
     function getSourceUrl()
     {
         return $this->sourceUrl;
--- getXML.php
+++ getXML.php
@@ -2,8 +2,8 @@
 include 'src/Rss.php';
 use src\Rss;
 
-define('DEF_URL', 'https://fr.news.yahoo.com/?nf=1&format=rss');
-        
+define('DEF_URL', 'https://fr.news.yahoo.com/?format=rss');
+
 $url = isset($_GET['url']) && !empty($_GET['url']) ? (string) $_GET['url'] : DEF_URL;
 $rss = new Rss($url);
 $rss->retrieveXml()->getXmlFile();
